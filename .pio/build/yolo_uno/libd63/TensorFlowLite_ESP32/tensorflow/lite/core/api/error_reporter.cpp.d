33638d9724c634c8f1ece9e28eadbfcc46f2a4b0
Hồ Ngọc Anh Tuấn <tuan.hona2004@hcmut.edu.vn>
2026-01-28 11:01:30 +0700

    no message

--- .pio/build/yolo_uno/libd63/TensorFlowLite_ESP32/tensorflow/lite/core/api/error_reporter.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/yolo_uno/libd63/TensorFlowLite_ESP32/tensorflow/lite/core/api/error_reporter.cpp.o: \
- .pio/libdeps/yolo_uno/TensorFlowLite_ESP32/src/tensorflow/lite/core/api/error_reporter.cpp \
- .pio/libdeps/yolo_uno/TensorFlowLite_ESP32/src/tensorflow/lite/core/api/error_reporter.h
--- .pio/build/yolo_uno/libeb3/Adafruit NeoPixel/kendyte_k210.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/yolo_uno/libeb3/Adafruit\ NeoPixel/kendyte_k210.c.o: \
- .pio/libdeps/yolo_uno/Adafruit\ NeoPixel/kendyte_k210.c
